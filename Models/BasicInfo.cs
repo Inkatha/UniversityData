171b4f5d567696471b21bca9746b85a60f7af92a
Malik Inkatha <minkatha01@gmail.com>
2016-12-27 11:49:52 -0600

    Add basic models

---
+++ Models/BasicInfo.cs
@@ -0,0 +1,11 @@
+namespace UniversityData.Models
+{
+    public class BasicInfo
+    {
+        public int Id { get; set; }
+        public int OpeId { get; set; }
+        public int OpeId6 { get; set; }
+        public string UniversityName { get; set; }
+        public string UniversityUrl { get; set; }
+    }
+}
